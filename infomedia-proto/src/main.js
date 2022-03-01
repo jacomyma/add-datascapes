760acc94c9244ca372a3483dbd0e1f1109d7ccf5
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2022-03-01 14:40:49 +0100

    d3 + load data

--- infomedia-proto/src/main.js
+++ infomedia-proto/src/main.js
@@ -1,9 +1,33 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import * as d3 from 'd3'
 
 const app = createApp(App);
 
 app.use(router);
 
 app.mount("#app");
+
+// Load Infomedia CSV
+console.log("Logging data...")
+var data = {}
+d3.csv('/data/infomedia_raw.csv', row => {
+	var obj = {}
+	obj.id = row.duid
+	obj.text = row.full_text
+	obj.heading =	row.heading
+	/*
+	obj.links = []
+	try {
+		obj.links = JSON.parse(row.links)
+	} catch(e) {
+		console.warn(e)
+	}
+	*/
+	obj.date = row.publishdate
+	obj.source = row.sourcename
+	obj.year = +row.year
+	data[obj.id] = obj
+})
+console.log("...done.")
--- infomedia-proto/src/views/HomeView.vue
+++ infomedia-proto/src/views/HomeView.vue
@@ -2,7 +2,42 @@
 
 <template>
   <main style="display: flex; flex-grow:1;">
-    <div style="background-color: #FF0; flex-grow:1;">Left</div>
-    <div style="background-color: #0FF; width: 600px;">Right</div>
+    <div style="flex-grow:1; display: flex; flex-direction: column;">
+      <div style="
+        background-color: #444;
+        height: 200px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      ">
+        <div>
+          Header (query field)
+        </div>
+      </div>
+      <div style="
+        background-color: #333;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      ">
+        <div>
+          Basemap
+        </div>
+      </div>
+    </div>
+    <div style="
+      width: 600px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    ">
+      <div>
+        Document results
+      </div>
+    </div>
   </main>
 </template>
