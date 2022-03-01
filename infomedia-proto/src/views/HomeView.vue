7edc83f83c1015711aadd54962263303aa297cfc
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2022-03-01 14:05:36 +0100

    Basic layout

--- infomedia-proto/src/views/HomeView.vue
+++ infomedia-proto/src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <script setup></script>
 
 <template>
-  <main>hein?</main>
+  <main style="display: flex; flex-grow:1;">
+    <div style="background-color: #FF0; flex-grow:1;">Left</div>
+    <div style="background-color: #0FF; width: 600px;">Right</div>
+  </main>
 </template>
