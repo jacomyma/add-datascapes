db533eaf329366ec82751b96851919858393c8f5
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2023-01-10 12:38:51 +0100

    setup

--- scopus2/vite.config.js
+++ scopus2/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // EDIT ME TO CHANGE THE DATASCAPE FOLDER
-const base = '/infomedia/';
+const base = '/scopus2/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
